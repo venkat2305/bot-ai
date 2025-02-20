5888fd71a47f253030c7a542255924b391f1297e
venkat2305 <heliobvsr2002@gmail.com>
2025-02-20 11:13:06 +0530

    remove waste desc

--- src/components/ConversationStarter.js
+++ src/components/ConversationStarter.js
@@ -4,7 +4,6 @@ function ConversationStarter({ question, subtext, onAsk }) {
     return (
         <Card hoverable onClick={() => onAsk(question)} style={{background : "#d7c7f4"}}>
             <Typography.Title level={5}>{question}</Typography.Title>
-            {/* <Typography.Text style={{ fontFamily: 'Open Sans' }} type="secondary">{subtext}</Typography.Text> */}
         </Card>
     )
 }
